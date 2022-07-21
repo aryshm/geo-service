3c637615aee3ccd9ecb2a2b3c6ef608dfa8e9bc2
aryshm <arishensky@yandex.ru>
2022-07-21 14:55:34 +0400

    @ParameterizedTest added

--- src/test/java/ru/netology/geo/TestGeoServiceImpl.java
+++ src/test/java/ru/netology/geo/TestGeoServiceImpl.java
@@ -1,17 +1,29 @@
 package ru.netology.geo;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 
+import java.util.stream.Stream;
+
 public class TestGeoServiceImpl {
 
-    @Test
-    void test_by_ip() {
+    @ParameterizedTest
+    @MethodSource("source")
+    void test_by_ip(String ip, Location expected) {
         GeoService geoService = new GeoServiceImpl();
-        Location actual = geoService.byIp("172.0.32.11");
-        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        Location actual = geoService.byIp(ip);
         Assertions.assertEquals(expected.getCountry(), actual.getCountry());
+    }
+    private static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of("172.0.32.11",
+                        new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
+                Arguments.of("96.44.183.149",
+                        new Location("New York", Country.USA, " 10th Avenue", 32))
+        );
     }
 }
--- src/test/java/ru/netology/i18n/TestLocalizationServiceImpl.java
+++ src/test/java/ru/netology/i18n/TestLocalizationServiceImpl.java
@@ -1,17 +1,26 @@
 package ru.netology.i18n;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.netology.entity.Country;
+
+import java.util.stream.Stream;
 
 public class TestLocalizationServiceImpl {
 
-    @Test
-    void testLocal() {
+    @ParameterizedTest
+    @MethodSource("source")
+    void testLocal(Country country, String expected) {
         LocalizationService localizationService = new LocalizationServiceImpl();
-        String actual = localizationService.locale(Country.RUSSIA);
-        String expected = "Добро пожаловать";
+        String actual = localizationService.locale(country);
         Assertions.assertEquals(expected, actual);
     }
-
+    private static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of(Country.RUSSIA, "Добро пожаловать"),
+                Arguments.of(Country.USA, "Welcome")
+        );
+    }
 }
